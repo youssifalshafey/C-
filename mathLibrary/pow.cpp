8295f13b6c02db9e86fa25d515425f8c639bc6f7
Youssif Alshafey <youssifalshafey@gmail.com>
2024-12-29 23:39:53 +0200

    math

---
+++ mathLibrary/pow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    short age;
+    cout << "Enter Your Age : ";
+    cin >> age;
+    cout << "Your Age Is " << pow(age, 2) << endl;
+    return 0;
+}
